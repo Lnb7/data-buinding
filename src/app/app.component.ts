45c050e5b08a80dd5222693dda8562d81d108263
loknathbanerjee <lokabanerjee@gmail.com>
2023-08-11 18:48:26 +0530

    data binding initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'data-buinding';
+
+  serverEelements = [{type: 'server',name: 'TestServer', content: 'just a test'}]
+
+  onServerAdded(serverData : {serverName: string, serverContent: string}){
+    this.serverEelements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent
+    });
+  }
+
+  onBlueprintCreated(blueprintData: {serverName: string, serverContent: string}){
+    this.serverEelements.push({
+      type: 'blueprint',
+      name: blueprintData.serverName,
+      content: blueprintData.serverContent
+    });
+  }
 }
